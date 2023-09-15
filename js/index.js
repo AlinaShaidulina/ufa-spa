7add4c47c52025d32601699e0a14d6a78f9df566
Andrei Storozhko <storanddev@gmail.com>
2023-09-15 14:41:17 +0500

    desktop

---
+++ js/index.js
@@ -0,0 +1,55 @@
+const menu = document.querySelector(".j-menu");
+const navbar = document.querySelector(".j-navbar");
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle("is-open");
+});
+
+// new Glide('.j-service-carousel', {
+//     type: 'carousel',
+//     startAt: 0,
+//     perView: 3,
+//     breakpoints: {
+//         767: {
+//             perView: 1,
+//         },
+//         991: {
+//             perView: 2,
+//         }
+//     },
+//     gap: 10,
+//     swipeThreshold: 20,
+// }).mount()
+new Glide('.j-greetings-carousel', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    gap: 10,
+    animationDuration: 500,
+    autoplay: 6000,
+    swipeThreshold: 20,
+}).mount()
+
+new Glide('.j-photo-carousel', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    gap: 10,
+    animationDuration: 800,
+    autoplay: 5000,
+    swipeThreshold: 20,
+    keyboard: true,
+}).mount()
+
+new Glide('.j-pros-carousel', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 5,
+    gap: 10,
+    animationDuration: 800,
+    autoplay: 2000,
+    swipeThreshold: 20,
+    keyboard: true,
+}).mount()
+
+
